bc605011126b63031361133932b9e6cad6f02186
StarkovA <artem.starkov@contractor.mattson.com>
2026-08-23 10:09:58 +0200

    Speed up steering animation

--- Assets/_Project/Scripts/MovementController.cs
+++ Assets/_Project/Scripts/MovementController.cs
@@ -6,12 +6,14 @@ public class MovementController : MonoBehaviour, IMoveSubscriber
     [SerializeField] private float _speed = 10f;
     [SerializeField] private float _acceleration = 25;
     [SerializeField] private float _rotationSpeed = 20;
+    [SerializeField] private float _animationSpeed = 5f;
 
     [SerializeField] private float _gravity = -9.81f;
     private GameInput _gameInput;
     private AnimationController _animationController;
     private float _currentSpeed = 0f;
     private Vector3 _gravityMovement;
+    private float _currentAnimationSpeed = 0f;
 
     public void Init(GameInput gameInput, AnimationController animationController)
     {
@@ -33,26 +35,31 @@ public class MovementController : MonoBehaviour, IMoveSubscriber
         transform.Rotate(Vector3.up * yRotation);
 
         float targetSpeed = 0;
+        float targetAnimationSpeed = 0;
         if (movement.y > 0)
         {
             targetSpeed = hasRotate ? _speed * 0.5f : _speed;
+            targetAnimationSpeed = hasRotate ? _animationSpeed * 0.5f : _animationSpeed;
         }
         else if (movement.y < 0)
         {
             targetSpeed = hasRotate ? -_speed * 0.5f : -_speed;
+            targetAnimationSpeed = hasRotate ? -_animationSpeed * 0.5f : -_animationSpeed;
         }
         else
         {
             targetSpeed = 0;
+            targetAnimationSpeed = 0;
         }
 
         var localMove = new Vector3(movement.x, 0, movement.y);
         _currentSpeed = Mathf.MoveTowards(_currentSpeed, targetSpeed, _acceleration * Time.deltaTime);
+        _currentAnimationSpeed = Mathf.MoveTowards(_currentAnimationSpeed, targetAnimationSpeed, _acceleration * Time.deltaTime);
 
         var globalMovement = transform.TransformDirection(localMove);
 
         _characterController.Move(globalMovement * Mathf.Abs(_currentSpeed) * Time.deltaTime);
-        _animationController.UpdateSpeed(_currentSpeed);
+        _animationController.UpdateSpeed(_currentAnimationSpeed);
 
         _gravityMovement.y += _gravity * Time.deltaTime;
         _characterController.Move(_gravityMovement * Time.deltaTime);
