11e5b9da469a50335437392707b853fdc400b2d6
StarkovA <artem.starkov@contractor.mattson.com>
2026-08-23 17:49:18 +0200

    Add soundable items, add sounds for them

---
+++ Assets/_Project/Scripts/InteractSoundItem.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class InteractSoundItem : MonoBehaviour, IInteractable
+{
+    private AudioSource _audioSource;
+    [SerializeField] private AudioClip _clip;
+
+    private void Start()
+    {
+        _audioSource = gameObject.AddComponent<AudioSource>();
+        _audioSource.clip = _clip;
+    }
+
+    public void Interact(bool canInteract)
+    {
+        _audioSource.Stop();
+        _audioSource.Play();
+    }
+}
