151a19aa528ed5f6e4edd66f9c297ac1794b3061
Margarita Chebotareva <chebotareva.m.r@gmail.com>
2026-09-09 20:12:28 +0200

    Add adaptive probe volume

--- Assets/_Project/Scripts/LightMapController.cs
+++ Assets/_Project/Scripts/LightMapController.cs
@@ -1,47 +1,101 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class LightMapController : MonoBehaviour, ILookAroundSubscriber
 {
-    [SerializeField] private Texture2D[] _lightAO;
-    [SerializeField] private Texture2D[] _dirAO;
-    [SerializeField] private Texture2D[] _lightDirect;
-    [SerializeField] private Texture2D[] _dirDirect;
-
-    private LightmapData[] _aOData, _directData;
-
+    [SerializeField] private bool _isUseScenario = false;
+    [SerializeField] private string _dayScenarioName = "Day";
+    [SerializeField] private string _nightScenarioName = "Night";
+    
+    [SerializeField][Range(0f, 1f)] private float _timeOfDay = 0.25f;
+    [SerializeField] private float _timeSpeed = 0.02f;
+    [SerializeField] private Light _sunLight;
+    [SerializeField] private float _timeStep = 0.5f;
+    private float _startTime;
     private GameInput _gameInput;
-    public void Init(GameInput gameInput)
+    private ProbeReferenceVolume _probeVolume;
+
+    public void OnEnable()
     {
-        return;
-        _gameInput = gameInput;
-        _gameInput.RegistrateLookAround(this);
-
-        _aOData = new LightmapData[_lightAO.Length];
-        for (int i = 0; i < _lightAO.Length; i++)
+        if (_isUseScenario)
         {
-            _aOData[i] = new LightmapData { lightmapColor = _lightAO[i], lightmapDir = _dirAO[i] };
-        }
-
-        _directData = new LightmapData[_lightDirect.Length];
-        for (int i = 0; i < _lightDirect.Length; i++)
-        {
-            _directData[i] = new LightmapData { lightmapColor = _lightDirect[i], lightmapDir = _dirDirect[i] };
+            _probeVolume = ProbeReferenceVolume.instance;
+            _probeVolume.lightingScenario = _nightScenarioName;
+            _probeVolume.lightingScenario = _nightScenarioName;
+            _sunLight.enabled = false;
         }
     }
-    public void OnLookAround(bool value)
+
+    public void Init(GameInput gameInput)
     {
-        if (value)
+        _gameInput = gameInput;
+        _gameInput.RegistrateLookAround(this);
+        _startTime = 0;
+        if (!_isUseScenario) return;
+    }
+
+    private void Update()
+    {
+        return;
+        if (_probeVolume == null)
         {
-            LightmapSettings.lightmaps = _aOData;
+            return;
+        }
+        _startTime += Time.deltaTime;
+        if (_startTime < _timeStep)
+        {
+            return;
         }
         else
         {
-            LightmapSettings.lightmaps = _directData;
+            _startTime = 0;
+        }
+
+        _timeOfDay += Time.deltaTime * _timeSpeed;
+        if (_timeOfDay > 1f)
+        {
+            _timeOfDay = 0f;
+        }
+
+        //float sunAngle = (_timeOfDay * 360f) - 135;
+        //_sunLight.transform.localRotation = Quaternion.Euler(sunAngle, 0, 0);
+
+        float blendWeight = 0f;
+
+        if (_timeOfDay >= 0.25f && _timeOfDay <= 0.75f)
+        {
+            blendWeight = Mathf.Abs(_timeOfDay - 0.5f) / 0.25f;
+        }
+        else
+        {
+            blendWeight = 1f;
+        }
+
+        blendWeight = Mathf.SmoothStep(0f, 1f, blendWeight);
+
+        _probeVolume.BlendLightingScenario(_nightScenarioName, blendWeight);
+    }
+
+    public void OnLookAround(bool value)
+    {
+        if (!_isUseScenario) return;
+        if (!value)
+        {
+
+            _probeVolume.BlendLightingScenario(_nightScenarioName, 0);
+            _sunLight.enabled = true;
+            //_timeOfDay -= 1;
+        }
+        else
+        {
+            _probeVolume.BlendLightingScenario(_nightScenarioName, 1);
+            _sunLight.enabled = false;
+            //_timeOfDay += 1;
         }
     }
 
     private void OnDestroy()
     {
-        //_gameInput.UnregistrateLookAround(this);
+        _gameInput.UnregistrateLookAround(this);
     }
 }
